c57c51bf79d11db3b4a07ccf9314e4036b35d5c6
Johan Källström <johka546@student.liu.se>
2012-11-16 18:13:01 +0100

    working testfile and animation skeleton, added fonts folder, font and a test image

--- src/testfiles/AnimatedSpriteTest.cc
+++ src/testfiles/AnimatedSpriteTest.cc
@@ -1,23 +1,51 @@
 #include "../AnimatedSprite.h"
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+ #include <SFML/Audio.hpp>
+ #include <SFML/Graphics.hpp>
+ 
+ int main()
+ {
+     // Create the main window
+     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+ 
+     // Load a sprite to display
+     sf::Texture texture;
+     if (!texture.loadFromFile("../../media/img/test.png"))
+         return EXIT_FAILURE;
+     AnimatedSprite sprite(texture);
+ 
+     // Create a graphical text to display
+     sf::Font font;
+     if (!font.loadFromFile("../../media/font/appleberry_with_cyrillic.ttf"))
+         return EXIT_FAILURE;
+     sf::Text text("Hello SFML", font, 50);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
-}
+     // Limit the framerate to 60 frames per second (this step is optional)
+     window.setFramerateLimit(60);
+ 
+     // Start the game loop
+     while (window.isOpen())
+     {
+         // Process events
+         sf::Event event;
+         while (window.pollEvent(event))
+         {
+             // Close window : exit
+             if (event.type == sf::Event::Closed)
+                 window.close();
+         }
+ 
+         // Clear screen
+         window.clear();
+ 
+         // Draw the sprite
+         window.draw(sprite);
+ 
+         // Draw the string
+         window.draw(text);
+ 
+         // Update the window
+         window.display();
+     }
+ 
+     return EXIT_SUCCESS;
+ }
