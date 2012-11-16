f4c55e21225cb4bf5e69475528b76c2a8629e2fd
david <mohlin.david@gmail.com>
2012-11-16 16:23:56 +0100

    startmenu samt fardigstallande av tidigare klasser

--- src/GameObjects/Menu/Menu.h
+++ src/GameObjects/Menu/Menu.h
@@ -9,13 +9,14 @@ class Menu : public GameObject
     public:
         Menu(int x, int y, class TextureLoader*, string textureReference);
         virtual ~Menu();
-        string readState()
-        bool isButtonClicked(int buttonIndex)
-        void drawSprite(RenderWindow)
-        virtual bool update()
+        string readState();
+        bool isButtonClicked(int buttonIndex);
+        bool isButtonPressed(int buttonIndex);
+        bool buttonGotPressed(int buttonIndex);
+        void drawMenu(RenderWindow);
+        virtual bool update();
         void addButton(Button newButton);
     protected:
-        sprite menuBackGround;
         string state;
         vector<Button> buttons;
     private:
