00d4c52d13fb814298618404413c6e761c1d1119
carka684 <carka684@student.liu.se>
2012-11-17 14:28:28 +0100

    La till mer i UI och la till mer eclipse-filer i .gitignore

---
+++ src/UI/MapMatix.cpp
@@ -0,0 +1,7 @@
+#include <string>
+#include "MapMatrix.h"
+
+
+MapMatrix::MapMatrix(std::string values, int x, int y)
+{
+}
---
+++ src/UI/MapMatrix.h
@@ -0,0 +1,12 @@
+#ifndef MAPMATRIX_H
+#define MAPMATRIX_H
+
+#include <string>
+
+
+class MapMatrix
+{
+    public:
+        MapMatrix(std::string value,int x, int y);
+}
+#endif
