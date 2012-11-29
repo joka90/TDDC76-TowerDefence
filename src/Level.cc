fca38238bc0b1c87bffdfc27df893450b0447502
Johan Källström <johka546@student.liu.se>
2012-11-29 13:29:48 +0100

    Level: Load player and level in the rigth order.

--- src/Level.cc
+++ src/Level.cc
@@ -63,6 +63,12 @@ Level::Level(string saveFile, TextureLoader& inTextures, SoundLoader& inSounds, 
 	char subType[20];
 	char parms[200];
 
+	//Temp variabels to load into	
+	int tmpLife;
+	int tmpMoney;
+	int tmpWave;
+	char tmpTrackFile[50];
+
 	FILE * pFile;
 
 	pFile = fopen(saveFile.c_str(),"r");
@@ -81,7 +87,7 @@ Level::Level(string saveFile, TextureLoader& inTextures, SoundLoader& inSounds, 
 				{
 					tmpPtr=new LongTower(parmsStr, textures, sounds, fonts);
 				}
-				cout << "New tower " << typeStr <<  " parms: " << parmsStr << endl;
+				cout << "New tower " << subTypeStr <<  " parms: " << parmsStr << endl;
 				//add tower if created
 				if(tmpPtr!=NULL)
 				{
@@ -90,23 +96,13 @@ Level::Level(string saveFile, TextureLoader& inTextures, SoundLoader& inSounds, 
 			}
 			else if(typeStr=="Level")
 			{
-				int tmpWave;
-				char tmpTrackFile[50];
 				sscanf(parms,"%i,%s",&tmpWave,tmpTrackFile);
-				//Init Level and wave, load from file
-				loadBase(string(tmpTrackFile),tmpWave);
-				cout << "Loading level " << tmpTrackFile <<  " wave: " << tmpWave << endl;
 			}
-			else if(typeStr=="Player")// TODO, fixa så att inte level kan råka skriva över player, ty players init poäng ligger där
+			else if(typeStr=="Player")
 			{
-				//Init Player
-				int tmpLife;
-				int tmpMoney;
 				sscanf(parms,"%i,%i",&tmpMoney,&tmpLife);
-				player=Player(tmpMoney, tmpLife);
-				cout << "Life " << tmpLife <<  " Money: " << tmpMoney << endl;
 			}
-			cout << "Type: " <<  type << "\tSubType: " <<  subType << "\tParameters: " << parms << endl;
+			//cout << "Type: " <<  type << "\tSubType: " <<  subType << "\tParameters: " << parms << endl;
 		}
 	}
 	else
@@ -114,6 +110,15 @@ Level::Level(string saveFile, TextureLoader& inTextures, SoundLoader& inSounds, 
 		cout << "error reading file: " << saveFile << endl;
 	}
 	fclose(pFile);
+
+	//Init Level and wave, load from file
+	loadBase(string(tmpTrackFile),tmpWave);
+	cout << "Loading level " << tmpTrackFile <<  " wave: " << tmpWave << endl;
+	
+	//init player after Level to overwrite life and money
+	player=Player(tmpMoney, tmpLife);
+	cout << "Life " << tmpLife <<  " Money: " << tmpMoney << endl;
+	
 }
 
 bool Level::update()
