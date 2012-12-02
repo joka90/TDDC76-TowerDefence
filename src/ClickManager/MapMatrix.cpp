14989b8f7dc1d956cf5e9a02f4f2d7f8587d7b0b
Johan Källström <johka546@student.liu.se>
2012-12-02 21:13:19 +0100

    MapMatrix: Fix segfault.

--- src/ClickManager/MapMatrix.cpp
+++ src/ClickManager/MapMatrix.cpp
@@ -46,13 +46,13 @@ void MapMatrix::setMatrix(string value, int row, int col)
 bool MapMatrix::isTaken(int pixelX, int pixelY)
 {
     pair<int,int>matrixPixel = convertPixelToMatrix(pixelX,pixelY);
-    if(matrixPixel.first<= getWidth() && matrixPixel.second <= getHeight())
+    if(matrixPixel.first < getWidth() && matrixPixel.second < getHeight())
 	{	
 		return !(matrix[matrixPixel.first][matrixPixel.second] == 0);
 	}
 	else
 	{
-		cout << "ERROR: Outside MapMatrix" << endl;
+		//cout << "ERROR: Outside MapMatrix" << endl;
 		return true;//To make it inpossible to place tower there
 	}
 }
@@ -60,7 +60,7 @@ bool MapMatrix::isTaken(int pixelX, int pixelY)
 void MapMatrix::setTower(int pixelX, int pixelY)
 {
 	pair<int,int>matrixPixel = convertPixelToMatrix(pixelX,pixelY);
-	if(matrixPixel.first<= getWidth() && matrixPixel.second <= getHeight())
+	if(matrixPixel.first < getWidth() && matrixPixel.second < getHeight())
 	{	
 		matrix[matrixPixel.first][matrixPixel.second] = 2; //Tower = 2
 	}
@@ -73,13 +73,13 @@ void MapMatrix::setTower(int pixelX, int pixelY)
 bool MapMatrix::isPath(int pixelX, int pixelY)
 {
     pair<int,int>matrixPixel = convertPixelToMatrix(pixelX,pixelY);
-    if(matrixPixel.first<= getWidth() && matrixPixel.second <= getHeight())
+    if(matrixPixel.first < getWidth() && matrixPixel.second < getHeight())
 	{	
 		return (matrix[matrixPixel.first][matrixPixel.second] == 1);
 	}
 	else
 	{
-		cout << "ERROR: Outside MapMatrix" << endl;
+		//cout << "ERROR: Outside MapMatrix" << endl;
 		return true;//To make it inpossible to place tower there
 	}
 }
