7863a0786640a998da62585ce16eee45886984af
Johan Källström <johka546@student.liu.se>
2012-12-03 12:08:24 +0100

    Upgrade menu: fixa tobbes merge

--- src/GameObjects/Menu/UpgradeMenu.cpp
+++ src/GameObjects/Menu/UpgradeMenu.cpp
@@ -6,7 +6,6 @@
 #define UPPGWIDTH 50
 #define UPPGHEIGHT 50
 #define UPPGBUTTON "BuyButton.png"
-<<<<<<< HEAD
 #define CLICKSOUND "CLICK.WAW"
 
 UpgradeMenu::UpgradeMenu(Player& inPlayer)
@@ -19,20 +18,6 @@ UpgradeMenu::~UpgradeMenu()
 {
     //dtor
 }
-=======
-#define CLICKSOUND "CLICK.WAW"
-
-UpgradeMenu::UpgradeMenu(Player& inPlayer)
- : Menu(UPPGMENUSTARTX, UPPGMENUSTARTY, "StartMenu.png"), player(inPlayer)
-{
-    //TODO
-}
-
-UpgradeMenu::~UpgradeMenu()
-{
-    //dtor
-}
->>>>>>> 92373cb6ee561863f2e3f71b1c513743f9651484
 
 bool UpgradeMenu::update()
 {
