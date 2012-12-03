e36ff39f7738a3f52bb189d5c8043fe2481731a8
David <mohlin.david@gmail.com>
2012-12-03 12:06:21 +0100

    destruktor på menu och uppgrade menu fixad, kör make clean om man har problem

---
+++ src/GameObjects/Menu/UpgradeMenu.cpp
@@ -0,0 +1,27 @@
+#include "UpgradeMenu.h"
+#define UPPGMENUSTARTX 500
+#define UPPGMENUSTARTY 0
+
+using namespace std;
+UpgradeMenu::UpgradeMenu(/*Player& inPlayer*/)
+: Menu(UPPGMENUSTARTX, UPPGMENUSTARTY, "StartMenu.png")//, selectedTower(NULL), player(inPlayer)
+{
+}
+
+UpgradeMenu::~UpgradeMenu()
+{
+}
+
+bool UpgradeMenu::upgrade()
+{
+    return true;
+}
+
+void UpgradeMenu::selectTower(Tower* inTower)
+{
+    //selectedTower = inTower;
+}
+void UpgradeMenu::deselectTower()
+{
+    //selectedTower = NULL;
+}
---
+++ src/GameObjects/Menu/UpgradeMenu.h
@@ -0,0 +1,22 @@
+#ifndef UPGRADEMENU_H
+#define UPGRADEMENU_H
+#include "../../ClickManager/Player.h"
+#include "../Towers/Tower.h"
+#include "Menu.h"
+
+
+class UpgradeMenu : public Menu
+{
+    public:
+        UpgradeMenu(/*Player& inPlayer*/);
+        virtual ~UpgradeMenu();
+        bool upgrade();
+        void selectTower(Tower* inTower);
+        void deselectTower();
+    protected:
+    private:
+        //Player& player;
+        //Tower* selectedTower;
+};
+
+#endif // UPGRADEMENU_H
