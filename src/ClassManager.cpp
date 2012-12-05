48efed5cf7bae20e287bc190eff10d6b09b62443
david <mohlin.david@gmail.com>
2012-12-05 17:19:11 +0100

    fiender tas bort

--- src/ClassManager.cpp
+++ src/ClassManager.cpp
@@ -38,7 +38,7 @@ Tower* ClassManager::createTowerInstance(const string& towerTypeName)
 Enemy* ClassManager::createEnemyInstance(const string& enemyTypeName)
 {
     if(enemyTypeName == "Badger"){
-            return new Enemy(100,10,500, "/Sprites/Badger/Gray/badger_01_small.png");
+            return new Enemy(100,2,500, "/Sprites/Badger/Gray/badger_01_small.png");
     }
     return NULL;
 }
